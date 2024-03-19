dcde698c395a1d9bd4b5c559461db76f19e16517
iho21 <hoivan08@gmail.com>
2024-03-18 17:42:49 -0700

    Spinners

--- app/src/main/java/com/example/customaryconversion/MainActivity.kt
+++ app/src/main/java/com/example/customaryconversion/MainActivity.kt
@@ -3,18 +3,70 @@ package com.example.customaryconversion
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+
 import androidx.core.view.WindowInsetsCompat
+import android.widget.*
+import androidx.core.view.WindowInsetsCompat.*
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val lengths = resources.getStringArray(R.array.Lengths)
+        val inSpinner = findViewById<Spinner>(R.id.inputSpinner)
+        if (inSpinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, lengths
+            )
+            inSpinner.adapter = adapter
+            object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.selected_item) + " " +
+                                "" + lengths[position], Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+
+                }
+            }
+            val outSpinner = findViewById<Spinner>(R.id.outputSpinner)
+
+            if (outSpinner != null) {
+                val adapter = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_item, lengths
+                )
+                outSpinner.adapter = adapter
+                object :
+                    AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>,
+                        view: View, position: Int, id: Long
+                    ) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            getString(R.string.selected_item) + " " +
+                                    "" + lengths[position], Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>) {
+                        // write code to perform some action
+
+                    }
+                }
+            }
         }
     }
 }
